73dc20cc80927168d7c3ffa1982092106f8b51e3
Swapniel <swapniel99@gmail.com>
2015-09-11 12:51:19 +0000

    Working prediction functions exported to PHP. Added Counter to keep track of number of predictons.

--- vowphpal_wabbit.cpp
+++ vowphpal_wabbit.cpp
@@ -5,20 +5,44 @@
  *  tell the compiler that the get_module is a pure C function
  */
 
-class Vowpal_Wabbit : public Php::Base
+class VowPHPal_Wabbit : public Php::Base
 {
     private:
         static void* _modelPointer;
+        static int _counter;
     public:
-        Vowpal_Wabbit() {}
-        virtual ~Vowpal_Wabbit() {}
+        VowPHPal_Wabbit() {}
+        virtual ~VowPHPal_Wabbit() {}
         static Php::Value getPrediction(Php::Parameters &params)
         {
-            return 0;
+            void* example = VW_ReadExampleA(_modelPointer, (char*)(params[0].rawValue()));
+            Php::Value score = VW_Predict(_modelPointer, example);
+            VW_FinishExample(_modelPointer, example);
+            _counter++;
+            return score;
+        }
+        static Php::Value getnPredictions(Php::Parameters &params)
+        {
+            std::vector<Php::Value> res;
+            std::vector<Php::Value> exampleArray = params[0];
+            for (Php::Value &exampleStr : exampleArray)
+            {
+                void* example = VW_ReadExampleA(_modelPointer, exampleStr.rawValue());
+                Php::Value score = VW_Predict(_modelPointer, example);
+                VW_FinishExample(_modelPointer, example);
+                res.push_back(score);
+                _counter++;
+            }
+            return (Php::Value)res;
+        }
+        static Php::Value getCounter()
+        {
+            return _counter;
         }
 };
 
-void* Vowpal_Wabbit::_modelPointer = NULL; //VW_InitializeA("-t -i model.vw");
+void* VowPHPal_Wabbit::_modelPointer = VW_InitializeA("--quiet -t -i /home/ec2-user/model.vw");
+int VowPHPal_Wabbit::_counter = 0;
 
 extern "C" {
     
@@ -36,10 +60,16 @@ extern "C" {
         static Php::Extension vwextension("vowphpal_wabbit", "1.0");
         
         // @todo    add your own functions, classes, namespaces to the extension
-        Php::Class<Vowpal_Wabbit> vowpal_wabbit("Vowpal_Wabbit");
-        vowpal_wabbit.method("getPrediction", &Vowpal_Wabbit::getPrediction);
+        Php::Class<VowPHPal_Wabbit> vowphpal_wabbit("VowPHPal_Wabbit");
+        vowphpal_wabbit.method("getPrediction", &VowPHPal_Wabbit::getPrediction, {
+                Php::ByRef("exampleString", Php::Type::String)
+                });
+        vowphpal_wabbit.method("getnPredictions", &VowPHPal_Wabbit::getnPredictions, {
+                Php::ByRef("exampleStringArray", Php::Type::Array)
+                });
+        vowphpal_wabbit.method("getCounter", &VowPHPal_Wabbit::getCounter);
 
-        vwextension.add(std::move(vowpal_wabbit));
+        vwextension.add(std::move(vowphpal_wabbit));
         
         // return the extension
         return vwextension;
